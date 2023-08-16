62e093f6f1f3ba03039cb039dff946fd3df2020f
Roy Ratcliffe <roy@ratcliffe.me>
2023-08-16 15:41:49 +0000

    fix: install python and requirements

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -2,7 +2,9 @@
 #
 #	cmake			3.27.2
 #	ninja			1.11.1
+#	python			3.11.4
 #	arm-none-eabi-gcc	13.2.0
 #
 # Automatically pours dependencies.
-brew install -q cmake ninja arm-none-eabi-gcc
+brew install -q cmake ninja python arm-none-eabi-gdb
+pip3 install -r Tools/setup/requirements.txt
